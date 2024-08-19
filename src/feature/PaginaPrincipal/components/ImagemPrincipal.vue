a4bba596fbd799d05c8fb806eb271365618929df
victorsantanna <victor.esef@hotmail.com>
2024-08-19 20:51:10 -0300

    feat:  cria o componente da imagem principal

---
+++ src/feature/PaginaPrincipal/components/ImagemPrincipal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="../../../assets/img/PaginaPrincipal/Imagem-principal.svg" alt="">
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+img{
+  width: 300px;
+  height: auto;
+}
+
+</style>
