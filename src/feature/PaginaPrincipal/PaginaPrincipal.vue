0600907a5746ee153d4b41fddd28e9a4a9417def
victorsantanna <victor.esef@hotmail.com>
2024-08-19 20:50:27 -0300

    feat: cria o componente da pagina principal

---
+++ src/feature/PaginaPrincipal/PaginaPrincipal.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div>
+            <CabecalhoProjeto />
+        </div>
+        <div class="conteudo-principal">
+            <ImagemPrincipal />
+            <ConteudoPrincipal />
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import CabecalhoProjeto from '../../components/CabecalhoProjeto.vue';
+import ImagemPrincipal from './components/ImagemPrincipal.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+</script>
+
+<style scoped>
+
+
+.conteudo-principal{
+    width: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+    margin: 0 auto;
+}
+
+@media (max-width: 768px) { /* Ajuste o valor conforme necessário */
+    .conteudo-principal {
+        flex-direction: column; /* Alinha os itens verticalmente quando a tela é pequena */
+        gap: 20px; /* Ajusta o espaçamento entre os itens em telas menores */
+        margin-top: 50px;
+    }
+
+    /* Esconde a imagem em telas pequenas */
+    .conteudo-principal > :nth-child(1) {
+        display: none;
+    }
+}
+</style>
