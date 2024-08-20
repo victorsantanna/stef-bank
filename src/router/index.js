c726a02540ca29a1cafbd856112d899008faf12c
victorsantanna <victor.esef@hotmail.com>
2024-08-20 20:59:36 -0300

    feat: configura a rota

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "login" */ '../feature/Login/LoginProjeto.vue')
+  },
+  {
+    path: '/cadastro',
+    name: 'cadastro',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "login" */ '../feature/Cadastro/CadastroProjeto.vue')
   }
 ]
 
