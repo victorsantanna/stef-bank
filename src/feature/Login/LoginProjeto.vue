2e3289f04bfbf106628a0dab84d6990ebe4eddb5
victorsantanna <victor.esef@hotmail.com>
2024-08-20 08:41:30 -0300

    feat: cria o componente

---
+++ src/feature/Login/LoginProjeto.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="formulario">
+      <ImagemLogin />
+      <div class="conteudo-formulario">
+        <FormularioLogin />
+      </div>
+      
+    </div>
+    <div>
+      <RodaPe />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ImagemLogin from './components/ImagemLogin.vue';
+import RodaPe from '@/components/RodaPe.vue';
+import FormularioLogin from './components/FormularioLogin.vue';
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.formulario {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  z-index: 1;
+}
+
+.conteudo-formulario{
+  margin-left: 200px;
+}
+
+
+</style>
