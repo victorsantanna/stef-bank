21364750c350a1ea2127514b672c4b1e222381a9
victorsantanna <victor.esef@hotmail.com>
2024-08-20 08:41:14 -0300

    feat: cria o componente

---
+++ src/components/RodaPe.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+div {
+    height: 70px;
+    background-color: var(--primary-color);
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+</style>
