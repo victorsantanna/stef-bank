24961244b03ac67f5b598dbb232fc8d5b4929319
victorsantanna <victor.esef@hotmail.com>
2024-08-20 20:58:43 -0300

    feat: cria o componente

---
+++ src/feature/Cadastro/CadastroProjeto.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <FormularioCadastro />
+        <ImagemCadastro />
+        <RodaPe />
+    </div>
+</template>
+
+<script setup>
+import RodaPe from '@/components/RodaPe.vue';
+import ImagemCadastro from './components/ImagemCadastro.vue';
+import FormularioCadastro from './components/FormularioCadastro.vue';
+</script>
+
+<style scoped></style>
